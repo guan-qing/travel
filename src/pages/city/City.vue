78d92d837944417a54062ba3715285cf3be0d273
guanqing <785252673@qq.com>
2018-10-24 16:37:12 +0800

    选择城市页开发头部

---
+++ src/pages/city/City.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="city">
+        <city-header></city-header>
+    </div>
+</template>
+
+<script>
+    import CityHeader from './components/Header'
+
+    export default {
+        name: "",
+        components: {
+            CityHeader
+        }
+    }
+</script>
+
+<style scoped lang="stylus" type="text/stylus">
+
+</style>
