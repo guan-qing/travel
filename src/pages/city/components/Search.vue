1c7e2bb55ad2e08eb851ebc0739715365ab35f15
guanqing <785252673@qq.com>
2018-10-24 16:59:14 +0800

    选择城市页搜索框

---
+++ src/pages/city/components/Search.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="search">
+        <input class="search-input" type="text" placeholder="输入城市名或拼音">
+    </div>
+</template>
+
+<script>
+    export default {
+        name: ""
+    }
+</script>
+
+<style scoped lang="stylus" type="text/stylus">
+    @import "~styles/varibles.styl";
+    .search
+        height .72rem
+        background $bgColor
+        padding .1rem
+        .search-input
+            box-sizing border-box
+            width 100%
+            height .62rem
+            line-height .62rem
+            text-align center
+            border-radius .06rem
+            color #666
+            padding 0 .1rem
+</style>
